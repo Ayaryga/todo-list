28896860f416d503d252840c81921958c42e3a66
Ayaryga <alekseyyaryga73@gmail.com>
2024-05-31 23:39:57 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css'; // Импортируем файл стилей
 import TodoList from './TodoList';
 import AddTodo from './AddTodo';
 
 const App = () => {
   const [todos, setTodos] = useState([]);
+
+  // Загрузка задач из localStorage при загрузке компонента
+  useEffect(() => {
+    const savedTodos = localStorage.getItem('todos');
+    if (savedTodos) {
+      setTodos(JSON.parse(savedTodos));
+    }
+  }, []);
+
+  // Сохранение задач в localStorage при изменении списка задач
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
 
   const addTodo = (text) => {
     const newTodos = [...todos, { text, id: Date.now() }];
